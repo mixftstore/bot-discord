24090c9d077fb66b0a91ea02300c358b1acb079e
mixftstore <123381898+mixftstore@users.noreply.github.com>
2023-01-23 20:59:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from myserver import server_on
 
-TOKEN = "MASUKKAN_BOT_TOKEN_ANDA" # ganti dengan token bot anda
+TOKEN = "MTA2NTk0MDI3MTE0NzI1Nzg2Nw.G5Svpc.hxcUNhkokt3vpOx--N6KpRygDKvtzlKxxpkogo" # ganti dengan token bot anda
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
@@ -15,4 +15,4 @@ async def hello(ctx):
     await ctx.send('Hello, {}! Jangan lupa subscribenya ya :)'.format(ctx.author.name.title()))
 
 server_on()
-bot.run(TOKEN)
+bot.run(MTA2NTk0MDI3MTE0NzI1Nzg2Nw.G5Svpc.hxcUNhkokt3vpOx--N6KpRygDKvtzlKxxpkogo)
